857dbe50d1c2bc0aa9d0ea955e7af39f32aea8a4
Pedro Costa <dev@iampfac.com>
2019-05-28 23:45:01 +0100

    Fix tests
    Why:
    
    * The private transaction tests are broken due to namespacing issues
      caused by 406004aa000517eb5c25fc4e4f28a7542376150d.
    * The `BaseEntity.from_collection` is failing because the test is not
      accounting to entities being converted to hashes before being passed
      to the constructor.
    
    This change addresses these issues by:
    
    * Updating references in the private transactions tests to specify the
      `Uphold` namespace;
    * Refactoring the `BaseEntity.from_collection` test to account for the
      conversion to hash prior to the object construction.

--- spec/unit/api/private_transaction_spec.rb
+++ spec/unit/api/private_transaction_spec.rb
@@ -3,22 +3,22 @@
 require 'spec_helper'
 
 describe Uphold::API::PrivateTransaction do
-  let(:client) { Client.new }
+  let(:client) { Uphold::Client.new }
 
   context '#create_transaction' do
     it 'creates a transaction for a specific card' do
       card_id = '1234'
-      request_data = RequestData.new(
-        Endpoints.with_placeholders(Endpoints::CARD_PRIVATE_TRANSACTIONS, ':card' => card_id),
-        Entities::Transaction,
+      request_data = Uphold::RequestData.new(
+        Uphold::Endpoints.with_placeholders(Uphold::Endpoints::CARD_PRIVATE_TRANSACTIONS, ':card' => card_id),
+        Uphold::Entities::Transaction,
         client.authorization_header,
         card_id: card_id, denomination: { currency: 'USD', amount: 10 }, destination: 'foo@bar.com'
       )
-      allow(Request).to receive(:perform_with_object)
+      allow(Uphold::Request).to receive(:perform_with_object)
 
       client.create_transaction(card_id: card_id, currency: 'USD', amount: 10, destination: 'foo@bar.com')
 
-      expect(Request).to have_received(:perform_with_object).
+      expect(Uphold::Request).to have_received(:perform_with_object).
         with(:post, request_data)
     end
   end
@@ -27,16 +27,16 @@ describe Uphold::API::PrivateTransaction do
     it 'commits a transaction for a specific card' do
       card_id = '1234'
       transaction_id = '4567'
-      request_data = RequestData.new(
-        Endpoints.with_placeholders(Endpoints::COMMIT_TRANSACTION, ':card' => card_id, ':id' => transaction_id),
-        Entities::Transaction,
+      request_data = Uphold::RequestData.new(
+        Uphold::Endpoints.with_placeholders(Uphold::Endpoints::COMMIT_TRANSACTION, ':card' => card_id, ':id' => transaction_id),
+        Uphold::Entities::Transaction,
         client.authorization_header
       )
-      allow(Request).to receive(:perform_with_object)
+      allow(Uphold::Request).to receive(:perform_with_object)
 
       client.commit_transaction(card_id: card_id, transaction_id: transaction_id)
 
-      expect(Request).to have_received(:perform_with_object).
+      expect(Uphold::Request).to have_received(:perform_with_object).
         with(:post, request_data)
     end
   end
@@ -44,17 +44,17 @@ describe Uphold::API::PrivateTransaction do
   context '#create_and_commit_transaction' do
     it 'creates a transaction for a specific card' do
       card_id = '1234'
-      request_data = RequestData.new(
-        Endpoints.with_placeholders(Endpoints::CREATE_AND_COMMIT_TRANSACTION, ':card' => card_id),
-        Entities::Transaction,
+      request_data = Uphold::RequestData.new(
+        Uphold::Endpoints.with_placeholders(Uphold::Endpoints::CREATE_AND_COMMIT_TRANSACTION, ':card' => card_id),
+        Uphold::Entities::Transaction,
         client.authorization_header,
         card_id: card_id, denomination: { currency: 'USD', amount: 10 }, destination: 'foo@bar.com'
       )
-      allow(Request).to receive(:perform_with_object)
+      allow(Uphold::Request).to receive(:perform_with_object)
 
       client.create_and_commit_transaction(card_id: card_id, currency: 'USD', amount: 10, destination: 'foo@bar.com')
 
-      expect(Request).to have_received(:perform_with_object).
+      expect(Uphold::Request).to have_received(:perform_with_object).
         with(:post, request_data)
     end
   end
@@ -63,16 +63,16 @@ describe Uphold::API::PrivateTransaction do
     it 'cancels a transaction for a specific card' do
       card_id = '1234'
       transaction_id = '4567'
-      request_data = RequestData.new(
-        Endpoints.with_placeholders(Endpoints::CANCEL_TRANSACTION, ':card' => card_id, ':id' => transaction_id),
-        Entities::Transaction,
+      request_data = Uphold::RequestData.new(
+        Uphold::Endpoints.with_placeholders(Uphold::Endpoints::CANCEL_TRANSACTION, ':card' => card_id, ':id' => transaction_id),
+        Uphold::Entities::Transaction,
         client.authorization_header
       )
-      allow(Request).to receive(:perform_with_object)
+      allow(Uphold::Request).to receive(:perform_with_object)
 
       client.cancel_transaction(card_id: card_id, transaction_id: transaction_id)
 
-      expect(Request).to have_received(:perform_with_object).
+      expect(Uphold::Request).to have_received(:perform_with_object).
         with(:post, request_data)
     end
   end
@@ -81,32 +81,32 @@ describe Uphold::API::PrivateTransaction do
     it 'resends a transaction for a specific card' do
       card_id = '1234'
       transaction_id = '4567'
-      request_data = RequestData.new(
-        Endpoints.with_placeholders(Endpoints::RESEND_TRANSACTION, ':card' => card_id, ':id' => transaction_id),
-        Entities::Transaction,
+      request_data = Uphold::RequestData.new(
+        Uphold::Endpoints.with_placeholders(Uphold::Endpoints::RESEND_TRANSACTION, ':card' => card_id, ':id' => transaction_id),
+        Uphold::Entities::Transaction,
         client.authorization_header
       )
-      allow(Request).to receive(:perform_with_object)
+      allow(Uphold::Request).to receive(:perform_with_object)
 
       client.resend_transaction(card_id: card_id, transaction_id: transaction_id)
 
-      expect(Request).to have_received(:perform_with_object).
+      expect(Uphold::Request).to have_received(:perform_with_object).
         with(:post, request_data)
     end
   end
 
   context '#all_user_transactions' do
     it 'lists all the transactions associated with the current user' do
-      request_data = RequestData.new(
-        Endpoints::USER_PRIVATE_TRANSACTIONS,
-        Entities::Transaction,
+      request_data = Uphold::RequestData.new(
+        Uphold::Endpoints::USER_PRIVATE_TRANSACTIONS,
+        Uphold::Entities::Transaction,
         client.authorization_header.merge(client.pagination_header_for_range(0..4))
       )
-      allow(Request).to receive(:perform_with_objects)
+      allow(Uphold::Request).to receive(:perform_with_objects)
 
       client.all_user_transactions
 
-      expect(Request).to have_received(:perform_with_objects).
+      expect(Uphold::Request).to have_received(:perform_with_objects).
         with(:get, request_data)
     end
   end
@@ -114,16 +114,16 @@ describe Uphold::API::PrivateTransaction do
   context '#all_card_transactions' do
     it 'lists all the transactions associated with a specific card' do
       card_id = '1234'
-      request_data = RequestData.new(
-        Endpoints.with_placeholders(Endpoints::CARD_PRIVATE_TRANSACTIONS, ':card' => card_id),
-        Entities::Transaction,
+      request_data = Uphold::RequestData.new(
+        Uphold::Endpoints.with_placeholders(Uphold::Endpoints::CARD_PRIVATE_TRANSACTIONS, ':card' => card_id),
+        Uphold::Entities::Transaction,
         client.authorization_header.merge(client.pagination_header_for_range(0..4))
       )
-      allow(Request).to receive(:perform_with_objects)
+      allow(Uphold::Request).to receive(:perform_with_objects)
 
       client.all_card_transactions(card_id: card_id)
 
-      expect(Request).to have_received(:perform_with_objects).
+      expect(Uphold::Request).to have_received(:perform_with_objects).
         with(:get, request_data)
     end
   end
--- spec/unit/entities/base_entity_spec.rb
+++ spec/unit/entities/base_entity_spec.rb
@@ -2,51 +2,48 @@
 
 require 'spec_helper'
 
-module Uphold
-  module Entities
-    describe BaseEntity do
-      class MockEntity < BaseEntity
-        attribute :a_key
-      end
+describe Uphold::Entities::BaseEntity do
+  class MockEntity < Uphold::Entities::BaseEntity
+    attribute :a_key
+  end
 
-      context '#initialize' do
-        it 'instantiates instance variables from arguments' do
-          entity = MockEntity.new(a_key: 'value')
+  context '#initialize' do
+    it 'instantiates instance variables from arguments' do
+      entity = MockEntity.new(a_key: 'value')
 
-          expect(entity.a_key).to eq 'value'
-        end
+      expect(entity.a_key).to eq 'value'
+    end
 
-        it 'converts camelCase to snake_case' do
-          entity = MockEntity.new(a_key: 'value')
+    it 'converts camelCase to snake_case' do
+      entity = MockEntity.new(a_key: 'value')
 
-          expect(entity.a_key).to eq 'value'
-        end
-      end
+      expect(entity.a_key).to eq 'value'
+    end
+  end
 
-      context '#error?' do
-        it 'is false for non-error entities' do
-          entity = Uphold::Entities::User.new
+  context '#error?' do
+    it 'is false for non-error entities' do
+      entity = Uphold::Entities::User.new
 
-          expect(entity.error?).to be_falsey
-        end
+      expect(entity.error?).to be_falsey
+    end
 
-        it 'is true for error entities' do
-          entity = Uphold::Entities::Error.new
+    it 'is true for error entities' do
+      entity = Uphold::Entities::Error.new
 
-          expect(entity.error?).to be_truthy
-        end
-      end
+      expect(entity.error?).to be_truthy
+    end
+  end
 
-      context '.from_collection' do
-        it 'instantiates each of the entities passed in' do
-          entity = double('Entity')
-          allow(MockEntity).to receive(:new)
+  context '.from_collection' do
+    it 'instantiates each of the entities passed in' do
+      h = double(Hash)
+      entity = double('Entity', to_h: h)
+      allow(MockEntity).to receive(:new)
 
-          MockEntity.from_collection([entity], '0-1/10')
+      MockEntity.from_collection([entity], '0-1/10')
 
-          expect(MockEntity).to have_received(:new).with(entity)
-        end
-      end
+      expect(MockEntity).to have_received(:new).with(h)
     end
   end
 end
